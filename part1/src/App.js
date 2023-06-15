bb9e45c40a35995253c22dc4c6e41dc612982822
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-06-15 00:51:19 -0400

    complete part 1.1, 1.2

---
+++ part1/src/App.js
@@ -0,0 +1,32 @@
+const Header = props => <h1>{props.course}</h1>
+const Total = props => <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+const Part = props => <p>{props.part} {props.exercises}</p>
+const Content = props => {
+	return (
+		<>
+			<Part part={props.part[0]} exercises= {props.exercises[0]}/>
+			<Part part={props.part[1]} exercises= {props.exercises[1]}/>
+			<Part part={props.part[2]} exercises= {props.exercises[2]}/>
+		</>
+	)
+}
+
+const App = () => {
+	const course = 'Half Stack application development'
+	const part1 = 'Fundamentals of React'
+	const exercises1 = 10
+	const part2 = 'Using props to pass data'
+	const exercises2 = 7
+	const part3 = 'State of a component'
+	const exercises3 = 14
+  
+	return (
+	  <div>
+		<Header course={course} />
+		<Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
+		<Total exercises={[exercises1, exercises2, exercises3]}/>
+	  </div>
+	)
+  }
+  
+  export default App
