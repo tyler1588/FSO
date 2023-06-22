f08fdcab2ea84dc0a0a57a9e4f7e2834fde35764
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-06-22 00:21:32 -0400

    complete 1.6

---
+++ part1b/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (type) => {
+    if (type === "good") setGood(good + 1)
+    else if (type === "neutral") setNeutral(neutral + 1)
+    else setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => handleClick("good")}>good</button>
+      <button onClick={() => handleClick("neutral")}>neutral</button>
+      <button onClick={() => handleClick("bad")}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
