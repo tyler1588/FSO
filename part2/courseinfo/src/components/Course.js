6802c88bac6a85bf33e4efcf6b8b63d64a38e700
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-06-30 15:43:29 -0400

    complete 2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,13 @@
 import Header from './Header'
 import Content from './Content'
+import NumExercises from './NumExercises'
 
 const Course = ({course}) => {
     return (
         <div>
-          <Header name={course.name} />
-          <Content courseParts={course.parts} />
+          <Header name={course.name}/>
+          <Content courseParts={course.parts}/>
+          <NumExercises courseParts={course.parts}/>
         </div>
       )
 }
